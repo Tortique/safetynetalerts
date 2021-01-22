45083230695dd30cf5adbf0a9471fd6eb0dd1a36
Tortique <adrien86190@yahoo.fr>
2021-01-22 14:18:14 +0100

    #2 Création des Model

--- src/main/java/com/safetynet/safetynetalerts/model/MedicalRecord.java
+++ src/main/java/com/safetynet/safetynetalerts/model/MedicalRecord.java
@@ -1,4 +1,38 @@
 package com.safetynet.safetynetalerts.model;
 
+import com.jsoniter.annotation.JsonProperty;
+import com.jsoniter.fuzzy.MaybeStringIntDecoder;
+import lombok.Data;
+
+@Data
 public class MedicalRecord {
+    @JsonProperty(decoder = MaybeStringIntDecoder.class)
+    private String firstName;
+
+    private String lastName;
+
+    private String birthDate;
+
+    private String medications;
+
+    private String allergies;
+
+    public MedicalRecord (String firstName, String lastName, String birthDate, String medications, String allergies) {
+        this.firstName= firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    @Override
+    public String toString() {
+        return "MedicalRecord{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", medications='" + medications + '\'' +
+                ", allergies='" + allergies + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/safetynet/safetynetalerts/model/Person.java
+++ src/main/java/com/safetynet/safetynetalerts/model/Person.java
@@ -1,4 +1,47 @@
 package com.safetynet.safetynetalerts.model;
 
+import com.jsoniter.annotation.JsonProperty;
+import com.jsoniter.fuzzy.MaybeStringIntDecoder;
+import lombok.Data;
+
+
+@Data
 public class Person {
+    @JsonProperty(decoder = MaybeStringIntDecoder.class)
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String city;
+
+    private String zip;
+
+    private String phone;
+
+    private String email;
+
+    public Person (String firstName, String lastName, String address, String city, String zip, String phone, String email) {
+        this.firstName= firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
