6ab31db101cb8fab661f7e414cfd57e912b439b3
Tortique <adrien86190@yahoo.fr>
2021-02-04 14:48:15 +0100

    #3 Création du Reader

--- src/main/java/com/safetynet/safetynetalerts/dao/JSONReader.java
+++ src/main/java/com/safetynet/safetynetalerts/dao/JSONReader.java
@@ -19,16 +19,18 @@ import java.util.Map;
 @Slf4j
 @Component
 public class JSONReader implements Reader {
-    private Any initRead() throws IOException {
-        String input = "src/main/resources/data.json";
-        byte[] bytesFile = Files.readAllBytes(new File(input).toPath());
-        JsonIterator iter = JsonIterator.parse(bytesFile);
-        Any any = iter.readAny();
-        return any;
+
+    public JSONReader() throws IOException {
     }
 
-    public List<Person> readPerson () throws IOException {
-        Any personAny = initRead().get("persons");
+    String input = "src/main/resources/data.json";
+    byte[] bytesFile = Files.readAllBytes(new File(input).toPath());
+    JsonIterator iter = JsonIterator.parse(bytesFile);
+    Any any = iter.readAny();
+
+
+    public List<Person> readPerson() {
+        Any personAny = any.get("persons");
         List<Person> persons = new ArrayList<>();
         for (Any person : personAny) {
             persons.add(new Person(person.get("firstName").toString(),person.get("lastName").toString(),person.get("address").toString(),person.get("city").toString(),
@@ -37,23 +39,22 @@ public class JSONReader implements Reader {
         return persons;
     }
 
-    public List<MedicalRecord> readMedicalRecord() throws IOException {
-        Any medicalAny = initRead().get("medicalrecords");
+    public List<MedicalRecord> readMedicalRecord() {
+        Any medicalAny = any.get("medicalrecords");
         List<MedicalRecord> medicalRecords = new ArrayList<>();
         medicalAny.forEach(medical -> medicalRecords.add(new MedicalRecord(medical.get("firstName").toString(),medical.get("lastName").toString(),
                 medical.get("birthdate").toString(), medical.get("medications").toString(), medical.get("allergies").toString())));
         return medicalRecords;
     }
 
-    public Map<String, FireStation> readFireStation() throws IOException {
-        Any fireStationAny = initRead().get("firestations");
+    public Map<String, FireStation> readFireStation() {
+        Any fireStationAny = any.get("firestations");
         Map<String, FireStation> fireStations = new HashMap<>();
-        fireStationAny.forEach(station -> {
-            fireStations.compute(station.get("station").toString(),
-                    (k, v) -> v == null ?
-                            new FireStation(station.get("station").toString()).addAddress(station.get("address").toString()) :
-                            v.addAddress(station.get("address").toString()));
-        });
+        fireStationAny.forEach(station -> fireStations.compute(station.get("station").toString(),
+                (k, v) -> v == null ?
+                        new FireStation(station.get("station").toString()).addAddress(station.get("address").toString()) :
+                        v.addAddress(station.get("address").toString())));
         return fireStations;
     }
+
 }
